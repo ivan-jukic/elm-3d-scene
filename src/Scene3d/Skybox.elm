45806eaee90500df8b5c14bc996110a8f6f8cb15
Ivan <ivan.jukic14@gmail.com>
2021-07-19 20:55:18 +0100

    Skybox feature --- Implemented new Skybox feature, and added an API for loading and setting the skybox texture.

---
+++ src/Scene3d/Skybox.elm
@@ -0,0 +1,51 @@
+module Scene3d.Skybox exposing
+    ( Skybox
+    , loadEquirectangular
+    )
+
+{-|
+
+@docs Skybox
+
+
+# Loading a Skybox texture
+
+@docs loadEquirectangular
+
+-}
+
+import Scene3d.Skybox.Protected exposing (Skybox(..))
+import Task exposing (Task)
+import WebGL.Texture
+
+
+{-| Loaded and ready to use `Skybox` texture.
+
+At the moment we are only supporting equirectangular textures, but there are
+considerations to support cube maps in the future as well.
+
+-}
+type alias Skybox =
+    Scene3d.Skybox.Protected.Skybox
+
+
+
+-- LOADING EQUIRECTANGULAR SKYBOX TEXTURE
+
+
+{-| Function which defines a task for loading an equirectangular texture,
+which can then be applied as a `Scene3d` skybox background.
+
+The first argument is the path (absolute or relative) to the texture.
+
+-}
+loadEquirectangular : String -> Task WebGL.Texture.Error Skybox
+loadEquirectangular =
+    Task.map EquirectTexture
+        << WebGL.Texture.loadWith
+            { magnify = WebGL.Texture.linear
+            , minify = WebGL.Texture.nearest
+            , horizontalWrap = WebGL.Texture.clampToEdge
+            , verticalWrap = WebGL.Texture.clampToEdge
+            , flipY = True
+            }
